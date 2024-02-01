b536c0e77d03de6d165832fe642d26d66bd97cd8
Vishnu Srivatsava <49309611+VishnuSrivatsava@users.noreply.github.com>
2024-02-01 11:46:58 +0530

    added scraper and analysis

--- popup.js
+++ popup.js
@@ -1,13 +1,43 @@
 document.getElementById('checkButton').addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.runtime.sendMessage(
-        {action: "checkSiteCategories", url: tabs[0].url}, 
-        function(response) {
-          document.getElementById('result').textContent = 
-            response.categoriesCount ? 
-            `This site is categorized as malicious by ${response.categoriesCount} sources.` : 
-            'This site is not categorized as malicious.';
-        }
-      );
+        chrome.tabs.executeScript(tabs[0].id, {
+            code: `
+                var urls = Array.from(new Set(Array.from(document.querySelectorAll('a'))
+                .map(a => a.href)))
+                .filter(url => url); // filter out empty urls
+                chrome.runtime.sendMessage({action: "storeUrls", urls: urls});
+                console.log(urls)
+            `
+        }, function() {
+            if (chrome.runtime.lastError) {
+                console.error(chrome.runtime.lastError.message);
+            }
+        });
     });
-  });
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "storeUrls") {
+        var urls = request.urls;
+        urls.forEach(function(url) {
+            chrome.runtime.sendMessage(
+                {action: "checkSiteCategories", url: url}, 
+                function(response) {
+                    var resultText = response.categoriesCount ? 
+                        `The URL ${url} is categorized as malicious by ${response.categoriesCount} sources.` : 
+                        `The URL ${url} is not categorized as malicious.`;
+                    
+                    // Create a new paragraph element
+                    var p = document.createElement('p');
+                    p.textContent = resultText;
+
+                    // Change the color of the text based on whether the URL is malicious
+                    p.style.color = response.categoriesCount ? 'red' : 'green';
+                    
+                    // Append the paragraph to the 'result' element
+                    document.getElementById('result').appendChild(p);
+                }
+            );
+        });
+    }
+});
